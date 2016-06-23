79f7d3afdfcecbf050d6be9c4f9b2f492af7bd61
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2016-06-23 12:53:12 +0300

    Updated version

--- src/com/budiyev/population/Launcher.java
+++ src/com/budiyev/population/Launcher.java
@@ -18,7 +18,7 @@
 package com.budiyev.population;
 
 public final class Launcher {
-    public static final String VERSION = "3.0.3";
+    public static final String VERSION = "3.0.4";
     private static volatile boolean sConsoleMode;
 
     public static void main(String[] args) {
